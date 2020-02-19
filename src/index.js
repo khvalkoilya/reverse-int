eac813900c694d327724805214133e4e0eeb2118
khvalkoilya <khvalko.ilya@gmail.com>
2020-02-20 00:07:37 +0300

    feat: add solve

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 module.exports = function reverse (n) {
-  
+    return n<0? Number(String(n).split("").slice(1).reverse().join("").replace(/^0+/,'')) //делаем строкой-в массив-забираем с первого все-поворачиваем-в строку-если будут нули в начале уберем----в номер
+    : Number(String(n).split("").reverse().join("").replace(/^0+/,''));
 }
